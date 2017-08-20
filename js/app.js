4a0bf80fb47b6b332bad22ae4537ac17b4e1d6cf
Yevinn <Yevinn@Yevinn-Pro-3.local>
2017-08-20 08:27:09 -0600

    Adding and overlay and hidding on click

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an lightbox with the large image
 
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+//An image to overlay
+$overlay.append($image);
+//  Add overlay to overlay
+$("body").append($overlay);
+  //An image
+  //A caption
+
+
 //1- Capture the click even on a link to an image
 $("#imageGallery a").click(function(){
   event.preventDefault();
   var href = $(this).attr("href");
-  console.log(href);
-  //1.1- Show the overlay.
-  //1.2 - Update overlay with the image link
-  //1.3 - Get Childs Alt atribute and set caption
+
+  //Update overlay with the image link
+  $image.attr("src", href);
+  //Show the overlay.
+$overlay.show();
+  //Get Childs Alt atribute and set caption
 });
 
-//2 - Add overlay
-  //2.1 - An image
-  //2.2 - A caption
+
 //3 - When overlay is clicked
+$overlay.click(function(){
+  $overlay.hide();
+});
   //3.1 - Hide the overlay
