7fd870d799040ff8eeafb05d92bdad17b70cf891
Yevinn <Yevinn@Yevinn-Pro-3.local>
2017-08-20 07:57:40 -0600

    prevented normal link from loading

--- js/app.js
+++ js/app.js
@@ -2,9 +2,15 @@
 //Solution: Create an lightbox with the large image
 
 //1- Capture the click even on a link to an image
+$("#imageGallery a").click(function(){
+  event.preventDefault();
+  var href = $(this).attr("href");
+  console.log(href);
   //1.1- Show the overlay.
   //1.2 - Update overlay with the image link
   //1.3 - Get Childs Alt atribute and set caption
+});
+
 //2 - Add overlay
   //2.1 - An image
   //2.2 - A caption
