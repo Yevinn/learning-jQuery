27ecba0221235d38dda6dafc065c1be425b2a677
Yevinn <kevin@yevinn.com>
2017-08-23 16:01:58 -0600

    Created function passwordEvent and added keyup to event!

--- js/app.js
+++ js/app.js
@@ -2,8 +2,20 @@
 //Solution: Hide and show them at appropriate times
 
 //Hide Hints
+$("form span").hide();
+
+function passwordEvent(){
+  if($(this).val().length > 8){
+      //Hide hint if valid
+      $(this).next().hide();
+  } else{
+      //else show hint
+      $(this).next().show();
+  }
+}
 
 //When even happens on password input
+$("#password").focus(passwordEvent).keyup(passwordEvent);
   //Find out if password is valid
     //Hide hint if valid
     //else show hint
@@ -12,5 +24,3 @@
   //Find out if password and confirmation match
     //Hide hint if matched
     //Show hint if not (else)
-
-    
