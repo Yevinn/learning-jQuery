76ecafed8dd275785c5ed00e2a3123d267cb434d
Yevinn <kevin@yevinn.com>
2017-08-23 15:40:26 -0600

    Formulated a plan

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 //Problem: Hints are shown even when form is valid
 //Solution: Hide and show them at appropriate times
+
+//Hide Hints
+
+//When even happens on password input
+  //Find out if password is valid
+    //Hide hint if valid
+    //else show hint
+
+//When event happens on confirmation input
+  //Find out if password and confirmation match
+    //Hide hint if matched
+    //Show hint if not (else)
+
+    
