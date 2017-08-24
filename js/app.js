ef18c7743d64795aacc919136588567d1741389d
Yevinn <kevin@yevinn.com>
2017-08-24 08:14:50 -0600

    Added Enable Submit button if password is valid

--- js/app.js
+++ js/app.js
@@ -3,34 +3,50 @@
 var $password = $("#password");
 var $confirmPassword = $("#confirm_password");
 
-
 //Hide Hints
 $("form span").hide();
 
+function isPasswordValid(){
+  return $password.val().length > 7;
+}
+
+function arePasswordsMatching(){
+  return $password.val() === $confirmPassword.val();
+}
+function canSubmit(){
+  return isPasswordValid() && arePasswordsMatching();
+}
 function passwordEvent(){
-  if($password.val().length > 7){
+  if(isPasswordValid()){
       //Hide hint if valid
       $password.next().hide();
   } else{
       //else show hint
       $password.next().show();
+      $("button").prop("disabled");
   }
 }
 
 function confirmPasswordEvent(){
-  if($password.val() === $confirmPassword.val()){
+  if(arePasswordsMatching()){
     $confirmPassword.next().hide();
   } else{
     $confirmPassword.next().show();
+    $("button").prop("disabled");
   }
 }
 
+function enableSubmitEvent(){
+  $("#submit").prop("disabled", !canSubmit());
+}
+
 //When even happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);;
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
 
 
 //When event happens on confirmation input
-$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
   //Find out if password and confirmation match
     //Hide hint if matched
     //Show hint if not (else)
+enableSubmitEvent();
