a2cc49156f23b2eecda0cd9ae40412e41c2b533b
Yevinn <kevin@yevinn.com>
2017-08-24 15:12:35 -0600

    Added custom new color to palette controls

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
 var color =$(".selected").css("background-color");
 
 //When clicking on control list items
-$(".controls li").click(function(){
+$(".controls").on("click", "li" ,function(){
   //Deselect sibling elements
   $(this).siblings().removeClass("selected");
   //Select clicked element
   $(this).addClass("selected");
   //cache current color
-  color =$(this).css("background-color");
+  color = $(this).css("background-color");
 });
 
 
@@ -25,15 +25,26 @@ function changeColor(){
   var r = $("#red").val();
   var g = $("#green").val();
   var b = $("#blue").val();
-  $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + " )");
+  $("#newColor").css("background-color", "rgb(" + r + ", " + g + ",  " + b + " )");
 }
 
   //When color sliders change
     $("input[type=range]").change(changeColor);
 
 //When add color is pressed
+$("#addNewColor").click(function(){
   //append the color to the conrols ul
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
   //select the new color
+  $newColor.click();
+
+
+  $("#colorSelect").hide();
+
+});
+
 
 //On mouse events on the canvas
   //Draw lines
