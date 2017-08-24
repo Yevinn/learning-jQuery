169d2ea70214eb8faaf99d471a8ec5b6d75bb984
Yevinn <kevin@yevinn.com>
2017-08-24 14:51:30 -0600

    Add color slider, and it changes bg color

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 //Problem: No user interaction cuases no change to application
 //Solution: When user interacts cause changes appropriately
-
+var color =$(".selected").css("background-color");
 
 //When clicking on control list items
+$(".controls li").click(function(){
   //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
   //Select clicked element
+  $(this).addClass("selected");
+  //cache current color
+  color =$(this).css("background-color");
+});
+
 
 //When new color is pressed
   //Show color select or hide color select
+$("#revealColorSelect").click(function(){
+  changeColor();
+  $("#colorSelect").toggle();
+});
+
+function changeColor(){
+  //update the color
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + " )");
+}
 
   //When color sliders change
-    //update the color
+    $("input[type=range]").change(changeColor);
 
 //When add color is pressed
   //append the color to the conrols ul
